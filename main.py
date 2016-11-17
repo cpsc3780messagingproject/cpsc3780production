31fd67a141f087ce8d1676aa9d95a300e86b12c8
TheRadEcks <themostradicalX@gmail.com>
2016-11-16 23:04:54 -0700

    This should be much cleaner.

---
+++ main.py
@@ -0,0 +1,9 @@
+
+# python stdlib imports
+import socket
+
+# 3party imports
+
+
+# local lib imports
+
---
+++ modules/message.py
@@ -0,0 +1,7 @@
+class message():
+    def __init__(self, sequence, type, source, destination, payload):
+        self.seq = sequence
+        self.type = type
+        self.source = source
+        self.destination = destination
+        self.payload = payload
