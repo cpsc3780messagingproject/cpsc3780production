46d1d44d60e2e63a14a8e3a393960b61b718878b
TheRadEcks <themostradicalX@gmail.com>
2016-12-06 19:40:30 -0700

    more message types

--- modules/message_factory.py
+++ modules/message_factory.py
@@ -2,11 +2,13 @@ from modules.message import Message
 
 def construct_message(message_type, sequence, senderid, targetid, message):
         MESSAGE_TYPES = { 
-        0: 'SRV', #used for server communication
-        1: 'SND', #used to send a message to server
-        2: 'GET', #used to receive all messages from server
-        3: 'USR', #indicates a server is sending a client a userlist
-        4: 'ASN' #server is assigning a value to a client
+        0:      'SRV', #used for client-to-server communication
+        1:      'SND', #used to send a message to server
+        2:      'GET', #used to receive all messages from server
+        3:      'USR', #indicates a server is sending a client a userlist
+        4:      'ASN', #server is assigning a value to a client
+        5:      'IDR', #client is first connecting; requesting id
+        10:     'OFF'  #client is signing off
         }
         seq_str = '{:0>3}'.format(sequence)
         id_str = '{:0>10}'.format(senderid)
