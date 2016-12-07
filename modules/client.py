31bdc10bea817965ce479290a5297b291d135a55
TheRadEcks <themostradicalX@gmail.com>
2016-12-06 20:58:37 -0700

    Many tuples fixed.

--- modules/client.py
+++ modules/client.py
@@ -14,27 +14,16 @@
 import time
 import socket
 import pickle
+import threading
 from modules.message import Message
 from modules.message_factory import construct_message
 
 class MessageClient():
     def __init__(self, server):
         self.messages = []
-        self.host = server
+        self.host = (server, 5000)
         self.mess_seq = 0
         self.id = 0
-        
-        """s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        #s.setdefaulttimeout(5)
-        raw_msg = raw_input("Please input a message to transmit: ")
-        wrapped_msg = construct_message("SND", raw_msg, 0) 
-        s.sendto(pickle.dumps(wrapped_msg), (server, 5000))
-        s.connect((self.host, 5000))
-        clientid = pickle.loads(s.recv(65536))#receive ID from server
-        print ("Your assigned ID is: ", clientid.payload())
-        self.id = clientid.payload()
-        userlist = pickle.loads(s.recv(65536))#receive and print the user list
-        print("Userlist: ", userlist.payload())"""
     
     def activate(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -42,19 +31,19 @@ class MessageClient():
         while True:
             if (self.id == 0):
                 wrapped_msg = construct_message(6, self.mess_seq, self.id, 0, "")
-                s.sendto(pickle.dumps(wrapped_msg), (self.host, 5000))
-                data = s.recvfrom(65536)
+                s.sendto(pickle.dumps(wrapped_msg), self.host)
+                data, self.host = s.recvfrom(65536)
                 unpickled_data = pickle.loads(data)
                 self.id = unpickled_data.destination
                 print("Your assigned ID is: ", unpickled_data.payload)
                 wrapped_msg = construct_message(3, self.mess_seq, self.id, 0, "")
-                s.sendto(pickle.dumps(wrapped_msg), (self.host, 5000))
-                data = s.recvfrom(65536)
+                s.sendto(pickle.dumps(wrapped_msg), self.host)
+                data, self.host = s.recvfrom(65536)
                 unpickled_data = pickle.loads(data)
                 wrapped_msg = construct_message(3, self.mess_seq, self.id, 0, "")
-                s.sendto(pickle.dumps(wrapped_msg), (self.host, 5000))
-                print(unpickled_data.payload)
+                s.sendto(pickle.dumps(wrapped_msg), self.host)
+                print("Userlist: ", unpickled_data.payload)
                 
             raw_msg = raw_input("Please input a message to transmit: ")
             wrapped_msg = construct_message(1, self.mess_seq, self.id, 0, raw_msg) 
-            s.sendto(pickle.dumps(wrapped_msg), (self.host, 5000))
+            s.sendto(pickle.dumps(wrapped_msg), self.host)
