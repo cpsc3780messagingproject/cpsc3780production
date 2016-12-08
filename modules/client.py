88d8d642b825dc5bf5e8d04299accafab4f2e498
TheRadEcks <themostradicalX@gmail.com>
2016-12-07 22:17:11 -0700

    :

--- modules/client.py
+++ modules/client.py
@@ -207,7 +207,7 @@ class MessageClient():
                         break #this will eventually be the server's way of signalling "end of messages" - probably won't be an empty payload tho
                     else:
                         self.messages[unpickled.data.seq] = unpickled_data
-                for key in self.messages
+                for key in self.messages:
                     print (self.messages[key].payload, "\n")
             
 
